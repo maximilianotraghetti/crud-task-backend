dd93bc5fe1a74c610b99cc16d3627d07f73be6bf
Maci <traghettimaximiliano64@gmail.com>
2024-08-12 12:36:41 -0300

    Se realizo un cambio

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -1,15 +1,20 @@
 import { ConnectDB } from "../config/db.js";
 
 export async function createTask(req, res) {
-    const con = await ConnectDB()
+    try {
+        const con = await ConnectDB();
+        
+        const sql = 'INSERT INTO tasks (`title`, `description`, `done`) VALUES (?, ?, ?)';
+        
+        const { title, description, done } = req.body;
 
+        const [result] = await con.execute(sql, [title, description, done]);
 
-    const sql = 'INSERT INTO tasks (title, description, done) SET VALUES (? )'
-
-    
-    con.execute(sql)
-
-    res.status(200).send("Task creada")
+        res.status(200).send("Tarea creada exitosamente");
+    } catch (error) {
+        console.error("Error al crear la tarea:", error);
+        res.status(500).send("Hubo un error al crear la tarea");
+    }
 }
 
 export async function getOneTask(req, res) {
@@ -40,10 +45,10 @@ export async function updateTask(req, res) {
     const con = await ConnectDB()
 
     const sql = 'UPDATE tasks SET title = ?, description = ? WHERE id = ?'
-    const {title, description} = req.body
-    const {id} = req.params
+    const { title, description } = req.body
+    const { id } = req.params
 
-   const [result] = await con.query('UPDATE tasks SET title = ?, description = ? WHERE id = ?', [title, description, id])
+    const [result] = await con.query('UPDATE tasks SET title = ?, description = ? WHERE id = ?', [title, description, id])
 
     const [rows] = await con.query('SELECT * FROM tasks WHERE id = ?', [id])
     res.status(200).json(rows[0])
@@ -54,7 +59,7 @@ export async function deleteTask(req, res) {
     const con = await ConnectDB()
 
     const sql = 'SELECT * FROM `tasks` WHERE id = ?'
-    const {id} = req.params;
+    const { id } = req.params;
 
     const [result] = await con.query('DELETE FROM tasks WHERE id = ?', [id]);
     res.status(204);
